5f975a23a5493c7abad896e4d42e3b09860602bb
Hân <hanhnph24141@fpt.edu.vn>
2026-05-29 20:23:06 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "/api": { target: "http://localhost:3001", changeOrigin: true },
+      "/api": { target: "https://geostory-0wfq.onrender.com", changeOrigin: true },
     },
   },
 });
